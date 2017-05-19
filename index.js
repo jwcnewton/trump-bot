a2f20e70de2658a32cc368a1fa24f1b9cf2c0bc4
jwcnewton <jwcnewton@gmail.com>
2017-05-19 22:36:55 +0100

    Remote OPBeat webhook

--- index.js
+++ index.js
@@ -2,12 +2,6 @@ const sentenceHandle = require('./src/sentenceHandle.js');
 const grammarHandle = require('./src/grammarHandle.js');
 const twitterHandle = require('./src/twitterHandle.js');
 
-var opbeat = require('opbeat').start({
-    appId: '55befae23a',
-    organizationId: process.env.OPBEAT_ORGANIZATION_ID,
-    secretToken: process.env.OPBEAT_TOKEN_SECRET
-});
-
 setInterval(function() {
     sentenceHandle.generateStatement().then((sentence) => {
         grammarHandle(sentence).then(function(data) {
